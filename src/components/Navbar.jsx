431acab7ea84948193af86502d5ae13c07dcb420
jadAhr <jadaharrar2@gmail.com>
2025-03-22 19:08:07 +0000

    portfolio

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import "../../src/css/main.css"
+
+
+
+
+function Navabr(){
+    return (
+        <div className="navbar" >
+        <a className="title" href="/">Jad.</a>
+            <ul id="nav-link">
+            </ul>
+        </div>
+    );
+}
+export default Navabr;
