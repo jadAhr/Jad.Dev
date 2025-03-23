a717fe1802562b835ce3cda879d9ea01d3a3d59a
Jad <142613875+jadAhr@users.noreply.github.com>
2025-03-23 00:23:51 +0000

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import "../../src/css/main.css"
 function Navabr(){
     return (
         <div className="navbar" >
-        <a className="title" href="/">Jad.</a>
+        <a className="title" href="">Jad.</a>
             <ul id="nav-link">
             </ul>
         </div>
